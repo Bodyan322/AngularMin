67ffb762f1dcf2497ec877b77c0b9963b1d6079a
Bodyan322 <bogdan3206@gmail.com>
2019-02-12 14:40:44 +0200

    directives, add derectives

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 (function() {
-  const directives = [{}];
+  const directives = [{
+    name: 'ng-model',
+    functions: []
+  },
+  {
+    name: 'ng-init',
+    functions: []
+  },
+  {
+    name: 'ng-app',
+    functions: []
+  },
+  {
+    name: 'ng-show',
+    functions: []
+  },
+  {
+    name: 'ng-make-short',
+    functions: []
+  },
+  {
+    name: 'ng-bind',
+    functions: []
+  }];
 
   const smallAngular = {
     directive() {
