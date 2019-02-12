6f0b52e8f1deca9f48d8b6415c0057923ea4dbfb
Bodyan322 <bogdan3206@gmail.com>
2019-02-12 11:24:26 +0200

    dev, add changes to start the project

--- src/index.js
+++ src/index.js
@@ -3,41 +3,15 @@
 
   const smallAngular = {
     directive() {
-      // do smth
+      return null;
     },
     compile(node) {
-      directives['ng-click'].forEach(cb => cb(node));
+      return null;
     },
     bootstrap(node) {
-      node.queryselectorAll('*').forEach(el => {
-        this.compile(el);
-      });
+      return null;
     }
   };
-
-  smallAngular.directive('ng-model', function(el) {
-    // do smth
-  });
-  smallAngular.directive('ng-click', function(el) {
-    // do smth
-  });
-
-  smallAngular.directive('ng-show', function(el) {
-    // do smth
-  });
-
-  smallAngular.directive('ng-hide', function(el) {
-    // do smth
-  });
   window.smallAngular = smallAngular;
 })();
 
-window.smallAngular.directive('make_short', function(el) {
-// do smth
-});
-
-
-// smallAngular.bootstrap(node) 
-//   mount app to this node
-//   or
-//   find data attr ng-app - mount app to this node
